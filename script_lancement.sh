354dc58dbf1eb8e4fc50bb64f70319700983a32a
Hugo Riffaud De <hugordet@gmail.com>
2025-04-09 15:15:21 +0200

    ok

---
+++ script_lancement.sh
@@ -0,0 +1,10 @@
+cd ./../Per-Sent-1-2
+module load conda
+conda activate venv
+
+cd ./../PLANTED
+
+sh TrainEval.sh test_Atos_lancement_3 config_test-Atomiser_Atos.yaml tiny
+
+
+
--- training/trainer.py
+++ training/trainer.py
@@ -192,34 +192,34 @@ class Model(pl.LightningModule):
         L_masks=[]
         img_s2,img_l7,img_mo,img_s1,img_al,date_s2,date_l7,date_mo,date_s1,date_al,mask_s2,mask_l7,mask_mo,mask_s1,mask_al,labels,frequency = batch
         
-        if self.config["trainer"]["S2"]:
+        if self.config["dataset"]["S2"]:
             tokens_s2,tokens_mask_s2=self.get_tokens(img_s2,date_s2,mask_s2,mode="optique",modality="s2")
             L_masks.append(tokens_mask_s2)
             L_tokens.append(tokens_s2)
         
-        if self.config["trainer"]["L7"]:
+        if self.config["dataset"]["L7"]:
             tokens_l7,tokens_mask_l7=self.get_tokens(img_l7,date_l7,mask_l7,mode="optique",modality="l7")
             L_masks.append(tokens_mask_l7)
             L_tokens.append(tokens_l7)
 
-        if self.config["trainer"]["MODIS"]:
+        if self.config["dataset"]["MODIS"]:
             tokens_mo,tokens_mask_mo=self.get_tokens(img_mo,date_mo,mask_mo,mode="optique",modality="modis")
             L_masks.append(tokens_mask_mo)
             L_tokens.append(tokens_mo)
 
-        if self.config["trainer"]["S1"]:
+        if self.config["dataset"]["S1"]:
             tokens_s1,tokens_mask_s1=self.get_tokens(img_s1,date_s1,mask_s1,mode="sar",modality="s1")
             L_masks.append(tokens_mask_s1)
             L_tokens.append(tokens_s1)
 
-        if self.config["trainer"]["ALOS"]:
+        if self.config["dataset"]["ALOS"]:
             tokens_al,tokens_mask_al=self.get_tokens(img_al,date_al,mask_al,mode="sar",modality="alos")
             L_masks.append(tokens_mask_al)
             L_tokens.append(tokens_al)
 
 
         tokens=torch.cat(L_tokens,dim=1)
-        tokens_mask=torch.cat([L_masks],dim=1)
+        tokens_mask=torch.cat(L_masks,dim=1)
 
 
             
