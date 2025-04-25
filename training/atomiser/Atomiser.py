17460df042d80c4455cf3758168ea43bd42a4bb6
Hugo Riffaud de Turckheim <hugordet@gmail.com>
2025-04-25 14:28:42 +0200

    ok

--- training/atomiser/Atomiser.py
+++ training/atomiser/Atomiser.py
@@ -122,22 +122,22 @@ class Atomiser(pl.LightningModule):
         ))
 
         # Build cross/self-attn layers
-        #self.layers = nn.ModuleList()
-        #for i in range(0):
-        #    cache_args = {'_cache': (i>0 and weight_tie_layers)}
-        #    # cross
-        #    cross_attn = get_cross_attn(**cache_args)
-        #    cross_ff   = get_cross_ff(**cache_args)
-        #    # self
-        #    self_attns = nn.ModuleList()
+        self.layers = nn.ModuleList()
+        for i in range(0):
+            cache_args = {'_cache': (i>0 and weight_tie_layers)}
+            # cross
+            cross_attn = get_cross_attn(**cache_args)
+            cross_ff   = get_cross_ff(**cache_args)
+            # self
+            self_attns = nn.ModuleList()
             
-        #    for j in range(self_per_cross_attn):
-        #        self_attns.append(nn.ModuleList([
-        #            get_latent_attn(**cache_args, key = j),
-        #            get_latent_ff(**cache_args, key = j)
-        #        ]))
+            for j in range(self_per_cross_attn):
+                self_attns.append(nn.ModuleList([
+                    get_latent_attn(**cache_args, key = j),
+                    get_latent_ff(**cache_args, key = j)
+                ]))
 
-        #    self.layers.append(nn.ModuleList([cross_attn, cross_ff, self_attns]))
+            self.layers.append(nn.ModuleList([cross_attn, cross_ff, self_attns]))
 
   
 
@@ -248,23 +248,23 @@ class Atomiser(pl.LightningModule):
 
 
         # cross & self layers
-        #for idx_depth,(cross_attn, cross_ff, self_attns) in enumerate(self.layers):
-        #    
-        #    # optionally prune
-        #    t, m = tokens, tokens_mask
-        #    if self.masking > 0:
-        #        t, m, idx = pruning(t, m, self.masking)
-        #    # cross-attn
-        #    x = cross_attn(x, context=t, mask=m) + x
-        #    x = cross_ff(x) + x
-        #    # restore tokens if pruned
-        #    if self.masking > 0:
-        #        tokens[:, idx] = t
-        #        tokens_mask[:, idx] = m
-        #    # self-attn blocks
-        #    for (sa, ff) in self_attns:
-        #        x = sa(x) + x
-        #        x = ff(x) + x
+        for idx_depth,(cross_attn, cross_ff, self_attns) in enumerate(self.layers):
+            
+            # optionally prunewith 
+            t, m = tokens, tokens_mask
+            if self.masking > 0:
+                t, m, idx = pruning(t, m, self.masking)
+            # cross-attn
+            x = cross_attn(x, context=t, mask=m) + x
+            x = cross_ff(x) + x
+            # restore tokens if pruned
+            if self.masking > 0:
+                tokens[:, idx] = t
+                tokens_mask[:, idx] = m
+            # self-attn blocks
+            for (sa, ff) in self_attns:
+                x = sa(x) + x
+                x = ff(x) + x
         
 
     
